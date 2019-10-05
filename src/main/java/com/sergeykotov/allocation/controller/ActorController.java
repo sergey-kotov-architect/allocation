286fcdbe33314492d5af1be02abe78bdbab4fe55
Sergey Kotov <sergey1kotov@protonmail.com>
2019-10-05 22:49:09 +0200

    add ActorController

---
+++ src/main/java/com/sergeykotov/allocation/controller/ActorController.java
@@ -0,0 +1,44 @@
+package com.sergeykotov.allocation.controller;
+
+import com.sergeykotov.allocation.domain.Actor;
+import com.sergeykotov.allocation.service.ActorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/actor")
+public class ActorController {
+    private final ActorService actorService;
+
+    @Autowired
+    public ActorController(ActorService actorService) {
+        this.actorService = actorService;
+    }
+
+    @PostMapping
+    public void create(@RequestBody Actor actor) {
+        actorService.create(actor);
+    }
+
+    @GetMapping
+    public List<Actor> getAll() {
+        return actorService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Actor getById(@PathVariable long id) {
+        return actorService.getById(id);
+    }
+
+    @PutMapping
+    public void update(@RequestBody Actor actor) {
+        actorService.update(actor);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable long id) {
+        actorService.deleteById(id);
+    }
+}
