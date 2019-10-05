e9b1aab4efc03dce449b8e2e816011343f1720c4
Sergey Kotov <sergey1kotov@protonmail.com>
2019-10-06 01:40:36 +0200

    fix VertexController request mapping

--- src/main/java/com/sergeykotov/allocation/controller/VertexController.java
+++ src/main/java/com/sergeykotov/allocation/controller/VertexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/vertex")
 public class VertexController {
     private final VertexService vertexService;
 
